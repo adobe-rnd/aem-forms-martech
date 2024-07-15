5e47161adc916cfcc38fc90fd7a164b92e02e580
Vijay Kumar J <jalagari@adobe.com>
2024-07-15 12:34:47 +0530

    Updating hero block

--- blocks/form/components/hero/hero.js
+++ blocks/form/components/hero/hero.js
@@ -1,4 +1,19 @@
-export default function decorate() {
-  // eslint-disable-next-line no-console
-  console.log('decorating form');
+import { subscribe } from '../../util.js';
+
+function updateImage(fieldDiv, field) {
+  console.log(field);
+}
+
+export default function decorateRange(fieldDiv, field) {
+  subscribe(fieldDiv, updateImage);
+  const { value, name } = field;
+  const imageUrl = value || field?.label.value;
+  const image = `
+    <picture>
+      <source srcset="${imageUrl}?width=2000&optimize=medium" media="(min-width: 600px)">
+      <source srcset="${imageUrl}?width=750&optimize=medium">
+      <img alt="${name}" src="${imageUrl}?width=750&optimize=medium">
+    </picture>`;
+  fieldDiv.innerHTML += image;
+  fieldDiv.classList.add('hero-input');
 }
